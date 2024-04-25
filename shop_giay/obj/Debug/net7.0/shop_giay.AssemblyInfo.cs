9477c12215cf35dbba076e210a6578a5bcbc0f0e
dacthu784 <125370292+dacthu784@users.noreply.github.com>
2024-04-25 22:31:01 +0700

    dá

--- shop_giay/obj/Debug/net7.0/shop_giay.AssemblyInfo.cs
+++ shop_giay/obj/Debug/net7.0/shop_giay.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d766cc23e2d1ec8dce64eb6dd994fbaf8ffec01")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
->>>>>>> 6794dd637936693cac6040777474edb876e72486
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae7e0add4e8c26f103285ef0c7bf873aff690df")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
