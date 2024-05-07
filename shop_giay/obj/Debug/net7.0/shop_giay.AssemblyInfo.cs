cc9f7b6b9fb6ff29fdb907691bb396abb7e15b5d
LuogVnThanh <149984712+LuogVnThanh@users.noreply.github.com>
2024-05-07 23:10:00 +0700

    ...

--- shop_giay/obj/Debug/net7.0/shop_giay.AssemblyInfo.cs
+++ shop_giay/obj/Debug/net7.0/shop_giay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6058e8aa8a021e25106659be7089fc84c5649438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shop_giay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
